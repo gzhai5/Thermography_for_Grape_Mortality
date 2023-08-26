edd6f999732e3ce81831cfc0cf5ea8650ec0f327
gzhai5 <2412804430@qq.com>
2023-08-26 14:19:04 -0400

    add npy to mp4 convertor

--- code/mp4_converter.py
+++ code/mp4_converter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import imageio
+import matplotlib.cm as cm
 
 def npy_to_mp4(npy_file, output_file, fps=24):
     # Load NPY file
@@ -7,6 +8,7 @@ def npy_to_mp4(npy_file, output_file, fps=24):
 
     # Get dimensions from the NPY data
     frames, height, width = npy_data.shape
+    print(frames)
 
     # Create an empty list to store frames
     frame_list = []
@@ -14,16 +16,22 @@ def npy_to_mp4(npy_file, output_file, fps=24):
     # Convert NPY frames to RGB images
     for i in range(frames):
         # Normalize the frame data to 0-255 range
-        frame = (255 * (npy_data[i] - npy_data.min()) / (npy_data.max() - npy_data.min())).astype(np.uint8)
+        frame = (npy_data[i] - npy_data.min()) / (npy_data.max() - npy_data.min())
+
+        # frame = (255*frame).astype(np.uint8)
+        # frame = np.stack((frame,) * 3, axis=-1)
+        # frame_list.append(frame)
+
         # Convert grayscale frame to RGB
-        frame = np.stack((frame,) * 3, axis=-1)
+        colored_frame = cm.hot(frame)[:, :, :3]
+        colored_frame = (255 * colored_frame).astype(np.uint8)
         # Append frame to the list
-        frame_list.append(frame)
+        frame_list.append(colored_frame)
 
     # Write frames to MP4 video file using imageio
     imageio.mimwrite(output_file, frame_list, fps=fps)
 
 # Example usage
-npy_file_path = 'path/to/input.npy'
-output_file_path = 'path/to/output.mp4'
+npy_file_path = './CW_C3_B3.npy'
+output_file_path = 'c3b3.mp4'
 npy_to_mp4(npy_file_path, output_file_path)
