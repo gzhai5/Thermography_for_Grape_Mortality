e2b2f6aead0c2c3ab3866fc8f7bec3703038e23c
gz244 <gz244@cornell.edu>
2024-01-14 21:35:31 -0500

    divide into multiple files; fix bugs

--- code/data_analysis/app_window.py
+++ code/data_analysis/app_window.py
@@ -1,55 +1,11 @@
-import sys
+import time
+from PyQt5 import QtWidgets, QtCore
+from mpl_canvas import MplCanvas
+from experiment_params import ExperimentParameter
+from data_process import find_top_sensitive_pixels, extract_mean_val
 import os
 import numpy as np
-from PyQt5 import QtWidgets, QtCore
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 
-class ExperimentParameter:
-    def __init__(self):
-        self.fps = 30
-        self.start_experiment_time = 0
-        self.start_heating_time = 1
-        self.end_heating_time = 10
-        self.end_experiment_time = 20
-
-        self.interested_points_num = 1000
-        self.selected_points = []
-        self.threshold = 9400
-        self.selected_point_radius = 5
-
-class MplCanvas(FigureCanvas):
-    def __init__(self, parent, width=5, height=4, dpi=100, modify_allowed=False, show_empty_first=False):
-        self.parent = parent
-        self.dpi = dpi
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super().__init__(fig)
-        if modify_allowed:
-            self.mpl_connect('button_press_event', self.on_click)
-        if show_empty_first:
-            self.display_empty()
-
-    def display_image(self, image_data):
-        self.axes.imshow(image_data, cmap='gray')  # Display as grayscale
-        self.axes.axis('off')  # Turn off axis
-        self.draw()
-
-    def display_empty(self):
-        # Create an empty black image
-        data = np.zeros((150, 150, 3), dtype=np.uint8)
-        self.axes.imshow(data)
-        self.axes.axis('off')
-
-        # Add text to the image
-        self.axes.text(0.5, 0.5, 'Please select the data file\n\n to read first',
-                       color='white', ha='center', va='center', transform=self.axes.transAxes)
-        self.draw()
-
-    def on_click(self, event):
-        if event.inaxes == self.axes:
-            x, y = int(event.xdata), int(event.ydata)
-            self.parent.on_image_click(x, y)
 
 class ThermalAnalysisApp(QtWidgets.QMainWindow):
     def __init__(self):
@@ -61,6 +17,7 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
         # Other variables
         self.data = None
         self.show_std_points = True
+        self.show_heatmap_flag = False
         self.current_folder = ""
         self.current_frame_index = 0
         self.params = ExperimentParameter()
@@ -76,43 +33,45 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
         grid_layout.setRowStretch(1, 1)
 
         # Top left: Image display
+        left_top_layout = QtWidgets.QVBoxLayout()
+        self.image_canvas = MplCanvas(self, modify_allowed=True, show_empty_first=True)
+        left_top_layout.addWidget(self.image_canvas)
 
         self.progress_bar = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.progress_bar.setEnabled(False)
+        self.progress_bar.setMinimumWidth(330)
         self.progress_bar.valueChanged.connect(self.on_frame_change)
         self.progress_info_label = QtWidgets.QLabel("0 / 0")
         self.progress_info_label.setAlignment(QtCore.Qt.AlignLeft)
+        progress_layout = QtWidgets.QHBoxLayout()
+        progress_layout.addWidget(self.progress_bar)
+        progress_layout.addWidget(self.progress_info_label)
+        left_top_layout.addLayout(progress_layout)
 
-        self.checkbox = QtWidgets.QCheckBox("Show Top N Sensistive Points")
-        self.checkbox.setChecked(True)
-        self.checkbox.setEnabled(False)
-        self.checkbox.stateChanged.connect(self.toggle_std_points)
+        self.checkbox_layout = QtWidgets.QHBoxLayout()
+        self.checkbox1 = QtWidgets.QCheckBox("Show Top N Sensistive Points")
+        self.checkbox1.setChecked(True)
+        self.checkbox1.setEnabled(False)
+        self.checkbox1.stateChanged.connect(self.toggle_std_points)
+        self.checkbox_layout.addWidget(self.checkbox1)
+        self.checkbox2 = QtWidgets.QCheckBox("Show Std Heatmap")
+        self.checkbox2.setChecked(False)
+        self.checkbox2.setEnabled(False)
+        self.checkbox2.stateChanged.connect(self.show_heatmap)
+        self.checkbox_layout.addWidget(self.checkbox2)
+        left_top_layout.addLayout(self.checkbox_layout)
 
         frame_buttons_layout = QtWidgets.QHBoxLayout()
-        frame_buttons_layout.setAlignment(QtCore.Qt.AlignTop)
+        frame_buttons_layout.setAlignment(QtCore.Qt.AlignLeft)
         frame_buttons_layout.setSpacing(10)
         self.revert_button = QtWidgets.QPushButton("Revert")
         self.revert_button.clicked.connect(self.revert_point_selection)
         self.revert_button.setEnabled(False)
-        frame_buttons_layout.addWidget(self.revert_button)
-
         self.next_button = QtWidgets.QPushButton("Next")
         self.next_button.clicked.connect(self.next_file)
         self.next_button.setEnabled(False)
+        frame_buttons_layout.addWidget(self.revert_button)
         frame_buttons_layout.addWidget(self.next_button)
-
-        self.image_canvas = MplCanvas(self, modify_allowed=True, show_empty_first=True)
-
-        left_top_layout = QtWidgets.QVBoxLayout()
-        left_top_layout.addWidget(self.image_canvas)
-        left_top_layout.addWidget(self.checkbox)
-
-        progress_layout = QtWidgets.QHBoxLayout()
-        progress_layout.addWidget(self.progress_bar)
-        progress_layout.addStretch(4)
-        progress_layout.addWidget(self.progress_info_label)
-        progress_layout.addStretch(1)
-        left_top_layout.addLayout(progress_layout)
         left_top_layout.addLayout(frame_buttons_layout)
 
         grid_layout.addLayout(left_top_layout, 0, 0)
@@ -193,7 +152,7 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
         if value:
             self.current_radius = int(value)
             self.params.selected_point_radius = int(value)
-            self.update_image_display()
+            self.update_raw_image_display(select_point_changed=True)
             self.radius_input.setText(f"{value}")
             self.update_plots()
 
@@ -221,7 +180,8 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
             self.refresh()
             self.load_image(file_path)
             self.progress_bar.setEnabled(True)
-            self.checkbox.setEnabled(True)
+            self.checkbox1.setEnabled(True)
+            self.checkbox2.setEnabled(True)
             self.threshold_input.setReadOnly(False)
             self.threshold_input.setText(f"{self.params.threshold}")
             self.top_n_input.setReadOnly(False)
@@ -244,19 +204,19 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
         self.params.selected_points.append((x, y))
         self.revert_button.setEnabled(True)
         self.update_plots()
-        self.update_image_display()
+        self.update_raw_image_display(select_point_changed=True)
 
     def update_image_display(self):
         self.update_raw_image_display()
-        self.update_heatmap_display()
+        self.update_heatmap_display() if self.show_heatmap_flag else self.heatmap_canvas.axes.clear()
 
-    def update_raw_image_display(self):
+    def update_raw_image_display(self, select_point_changed=False):
         frame = self.data[self.current_frame_index]
         self.image_canvas.axes.clear()  # Clear previous image/points
         self.image_canvas.axes.imshow(frame, cmap='gray')  # Display current frame
 
         if self.show_std_points:
-            top_points = self.find_top_sensitive_pixels(self.data, self.params.interested_points_num,
+            top_points = find_top_sensitive_pixels(self.data, self.params.interested_points_num,
                                                         self.params.start_heating_time * self.params.fps,
                                                         self.params.end_heating_time * self.params.fps,
                                                         self.params.threshold)
@@ -264,11 +224,10 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
             self.image_canvas.axes.plot(y_coords, x_coords, 'ro', markersize=1)  # Plot with red dots
 
         # Plot selected points
-        if self.params.selected_points:
+        if select_point_changed and self.params.selected_points:
             selected_x_coords, selected_y_coords = zip(*self.params.selected_points)
-            print(f'selected x: {selected_x_coords}, selected y: {selected_y_coords}')
-            point_diameter = 2 * self.params.selected_point_radius * (72 / self.image_canvas.dpi)
-            self.image_canvas.axes.plot(selected_x_coords, selected_y_coords, 'bo', markersize=point_diameter)  # Plot with blue dots
+            point_edge_length = 2 * self.params.selected_point_radius * (72 / self.image_canvas.dpi)
+            self.image_canvas.axes.plot(selected_x_coords, selected_y_coords, 'bs', markersize=point_edge_length)  # Plot with blue dots
 
             for idx, (x, y) in enumerate(self.params.selected_points):
                 self.image_canvas.axes.text(x, y, str(idx), color='orange', fontsize=8, ha='right', va='bottom')
@@ -306,9 +265,9 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
     def revert_point_selection(self):
         # Revert the order of selected points
         if self.params.selected_points:
-            self.params.selected_points = self.params.selected_points[::-1]
+            self.params.selected_points = self.params.selected_points[:-1]
             self.update_plots()
-            self.update_image_display()
+            self.update_raw_image_display(select_point_changed=True)
         else:
             self.revert_button.setEnabled(False)
 
@@ -336,12 +295,17 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
         self.show_std_points = (state == QtCore.Qt.Checked)
         self.update_image_display()
 
+    def show_heatmap(self, state):
+        # Toggle displaying heatmap
+        self.show_heatmap_flag = (state == QtCore.Qt.Checked)
+        self.update_image_display()
+
     def update_plots(self):
         if not self.params.selected_points:
             return
         
         # Extract mean and standard deviation values for selected points
-        mean_curves, std_curves = self.extract_mean_val(self.params.selected_points, self.data, self.params.selected_point_radius)
+        mean_curves, std_curves = extract_mean_val(self.params.selected_points, self.data, self.params.selected_point_radius)
 
         # Clear existing plots
         self.avg_plot_canvas.axes.clear()
@@ -369,60 +333,3 @@ class ThermalAnalysisApp(QtWidgets.QMainWindow):
         # Redraw the plots
         self.avg_plot_canvas.draw()
         self.std_plot_canvas.draw()
-
-    def find_top_sensitive_pixels(self, data: np.ndarray, top_n: int, start_frame: int, end_frame: int, threshold: int) -> list:
-        # Ensure the frame range is within the data bounds
-        start_frame = max(0, start_frame)
-        end_frame = min(data.shape[0], end_frame)
-
-        mean_values = np.mean(data[start_frame:end_frame, :, :], axis=0)
-        mask = self.mask_pixel_filter(mean_values, threshold)
-
-        # Apply the mask and calculate the standard deviation for the masked pixels
-        masked_data = data[start_frame:end_frame, :, :] * mask
-        std_dev = np.std(masked_data, axis=0)
-
-        # Flatten the std_dev array and get the indices of the top N std deviations
-        flat_std_dev = std_dev.flatten()
-        indices = np.argpartition(flat_std_dev, -top_n)[-top_n:]
-        sorted_indices = indices[np.argsort(flat_std_dev[indices])][::-1]
-
-        # Convert flat indices to 2D indices
-        x_coords, y_coords = np.unravel_index(sorted_indices, std_dev.shape)
-        return list(zip(x_coords, y_coords))
-    
-    # two methods apply mask to the data
-    # 1. mask the pixels with mean < threshold
-    # 2. mask the pixels with only the center of whole image
-    @staticmethod
-    def mask_pixel_filter(mean_values, threshold: int):
-        mask = mean_values < threshold
-        height, width = mean_values.shape
-        mask[:int(height/3), :] = False
-        mask[int(height*2/3):, :] = False
-        mask[:, :int(width/3)] = False
-        mask[:, int(width*2/3):] = False
-        return mask
-
-    
-    @staticmethod
-    def extract_mean_val(pixels: list, data: np.ndarray, radius: int):
-        mean_curves = []
-        std_curves = []
-        for (x, y) in pixels:
-            x_coor = int(x)
-            y_coor = int(y)
-            region = data[:, max(y_coor-radius, 0):min(y_coor+radius, data.shape[1]),
-                            max(x_coor-radius, 0):min(x_coor+radius, data.shape[2])]
-            mean_curve = np.mean(region, axis=(1, 2))
-            std_curve = np.std(region, axis=(1, 2))
-            mean_curves.append(mean_curve)
-            std_curves.append(std_curve)
-        return mean_curves, std_curves
-
-# Main execution
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = ThermalAnalysisApp()
-    main_window.show()
-    sys.exit(app.exec_())
